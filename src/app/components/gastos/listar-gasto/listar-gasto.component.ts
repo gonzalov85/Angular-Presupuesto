413f2e3cf91f6680dcdb59d00bf4b8fbe151f219
Gonzalo Villamandos <82946513+gonzalov85@users.noreply.github.com>
2021-12-07 12:21:54 -0300

    module conecions

---
+++ src/app/components/gastos/listar-gasto/listar-gasto.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { PresupuestoService } from 'src/app/services/presupuesto.service';
+
+@Component({
+  selector: 'app-listar-gasto',
+  templateUrl: './listar-gasto.component.html',
+  styleUrls: ['./listar-gasto.component.css']
+})
+export class ListarGastoComponent implements OnInit, OnDestroy {
+  subscription: Subscription;
+  presupuesto: number;
+  restante: number;
+
+
+  constructor(private _presupuestoService: PresupuestoService) {
+
+    this.presupuesto = 0;
+    this.restante = 0;
+
+    this.subscription = this._presupuestoService.getGastos().subscribe(data => {
+
+    })
+   }
+
+  ngOnInit(): void {
+    this.presupuesto = this._presupuestoService.presupuesto;
+    this.restante = this._presupuestoService.restante;
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+}
